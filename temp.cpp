9e47424eead450fbf2db311325348cc5b799acb5
ZZH <123837353+ZeHangZhu@users.noreply.github.com>
2023-07-27 13:12:28 +0800

    Add files via upload

--- temp.cpp
+++ temp.cpp
@@ -2,98 +2,128 @@
 * Copyright (C) 2023 ZeHangZhu
 * -*- coding:GBK -*-
 */
+
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
-class Temp
-{
+class Temp {
 	private:
 		/*缓存数据列表*/
 		string data;
-		
+
 		/*获得指定区域的字符串*/
 		string GetChars(int start,int end,string data);
-		
+
 		/*格式化字符串*/
 		string format(string data);
-		
+
 		/*反格式化字符串*/
 		string Unformat(string data);
-
 	public:
+
 		/*
 		* 在单元中增加新对象
 		* 支持的类型有 'char','int','float','bool',类型
 		*/
-		int AddNewObject(string type_obj,string value);
-		
+		int AddNewObject(string type_obj,string obj_name,string value);
+
 		/*单独读取单元中的数据*/
 		char GetObject(string type_name);
-		
+
 		/*储存到文件*/
 		int Save(string file);
-		
+
 };
 
-string Temp::GetChars(int start,int end,string data)
-{
+int Temp::Save(string file){
+	fstream fileobj;
+	fileobj.open(file.c_str(),ios::app);
+	fileobj<<data;
+}
+
+string Temp::GetChars(int start,int end,string data) {
 	string temp;
 	int i=start,j;
-	for(j=0; i<end; i++)
-		{
-			temp[j] = data[i];
-			j++;
-		}
+
+	for(j=0; i<end; i++) {
+		temp[j] = data[i];
+		j++;
+	}
+
 	return temp;
 }
 
-int Temp::AddNewObject(string type_obj,string value)
-{
+string Temp::format(string datas) {
+	string temp;
 
-	/*添加类型提示*/
-	if(type_obj == "char")
-		{
-			data = data + "c:";
+	const char* data;
+	data = datas.c_str();
+
+	int i = 0;
+	char ch;
+
+	//格式化
+	while(i<datas.length()) {
+		ch = data[i];
+		if(ch == '\"') {
+			temp += '^';
+			temp += '0';
+		} else if(ch == '^') {
+			temp += '^';
+			temp += '1';
+		} else {
+			temp += ch;
 		}
+		i++;
+	}
 
-	else if(type_obj == "bool")
-		{
-			data = data + "b:";
-		}
-
-	else if(type_obj == "int")
-		{
-			data = data+"i:";
-		}
-
-	else if(type_obj == "float")
-		{
-			data = data+"f:";
-		}
-	else
-		{
-			cout<<"LINE["<<__LINE__<<"]:类型不能为:"<<value <<endl;
-			return -2;
-		}
-
-	/*写入值*/
-	data += '\"';
-
-	int i=0;
-	while(1)
-		{
-			if(value[i] == '\0')
-				break;
-			if(value[i] == '\\')
-				break;
-		}
-
+	return temp;
 }
 
-char Temp::GetObject(string type_name)
-{
+string Temp::Unformat(string data) {
+	string temp;
+
+	return temp;
+}
+
+int Temp::AddNewObject(string type_obj,string obj_name,string value) {
+
+	/*添加类型提示*/
+	if(type_obj == "char") {
+		data = data + "c:";
+	}
+
+	else if(type_obj == "bool") {
+		data = data + "b:";
+	}
+
+	else if(type_obj == "int") {
+		data = data+"i:";
+	}
+
+	else if(type_obj == "float") {
+		data = data+"f:";
+	} else {
+		cout<<"LINE["<<__LINE__<<"]:类型不能为:"<<value <<endl;
+		return -2;
+	}
+	//写入名称
+	data += '[';
+	data += obj_name;
+	data += ']';
+	/*写入开头的引号*/
+	data += '\"';
+	/*写入值*/
+	data += format(value);
+	/*写入结尾的引号*/
+	data += "\"\n";
+	cout<<data<<endl;
+	return 0;
+}
+char Temp::GetObject(string type_name) {
 
 	char temp[40960];
 
@@ -102,48 +132,46 @@ char Temp::GetObject(string type_name)
 	/*设置起始位置*/
 	int start = i;
 	/*检查变量是否存在*/
-	if(i<0)
-		{
-			cout<<"变量["<<type_name<<"]不存在!"<<endl;
-			return '\0';
-		}
+	if(i<0) {
+		cout<<"变量["<<type_name<<"]不存在!"<<endl;
+		return '\0';
+	}
 
 	/*指针模式,1为类型,2为变量名,3为值,4用于判定结束*/
 	unsigned int flag=1;
-	while(1)
-		{
+	while(1) {
 
-			if(data[i] == '\0')
-				break;
+		if(data[i] == '\0')
+			break;
 
-			if(data[i] == ':')
-				flag = 2;
+		if(data[i] == ':')
+			flag = 2;
 
-			if(data[i] == '\"' && flag == 2)
-				flag = 3;
+		if(data[i] == '\"' && flag == 2)
+			flag = 3;
 
-			if(data[i] == '\"' && flag == 3)
-				flag = 4;
+		if(data[i] == '\"' && flag == 3)
+			flag = 4;
 
-			if(data[i] == ';' && flag == 4)
-				break;
+		if(data[i] == ';' && flag == 4)
+			break;
 
-			i++;
-		}
+		i++;
+	}
 
 	/*将数据段写入缓存*/
 
 }
 
-int test()
-{
+//正常测试
+int test() {
 	Temp a;
-	a.AddNewObject("wfsdwacf","dssa");	//异常测试
-	a.AddNewObject("int","qardh");	//正常测试
+	a.AddNewObject("char","test1","test\"^");
+	a.AddNewObject("char","test2","test2");
+	a.Save("1.txt");
 	return 0;
 }
 
-int main()
-{
+int main(int argc,char *argv[]) {
 	return test();
 }
